0c210e531f5f47761cc01e8eb12143575fddfa34
boommanpro <boommanpro@gmail.com>
2024-04-05 12:36:34 +0800

    test(add fail back test):

--- src/test/java/com/boommanpro/statemachine/instance/demo/OrderSateMachineTest.java
+++ src/test/java/com/boommanpro/statemachine/instance/demo/OrderSateMachineTest.java
@@ -1,6 +1,8 @@
 package com.boommanpro.statemachine.instance.demo;
 
 import com.alibaba.cola.statemachine.StateMachine;
+import com.alibaba.cola.statemachine.exception.TransitionFailException;
+import com.alibaba.cola.test.StateMachineTest;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -14,5 +16,7 @@ public class OrderSateMachineTest {
         OrderStateEnum orderStateEnum = stateMachine.fireEvent(OrderStateEnum.START, OrderEventEnum.INIT, new OrderStateMachineContext());
         Assert.assertEquals(orderStateEnum, OrderStateEnum.CREATE);
         System.out.println(stateMachine.generatePlantUML());
+        Assert.assertThrows(TransitionFailException.class,
+                () -> stateMachine.fireEvent(OrderStateEnum.END, OrderEventEnum.INIT, new OrderStateMachineContext()));
     }
 }
--- src/test/java/com/boommanpro/statemachine/instance/demo/OrderStateMachineBuilder.java
+++ src/test/java/com/boommanpro/statemachine/instance/demo/OrderStateMachineBuilder.java
@@ -1,8 +1,11 @@
 package com.boommanpro.statemachine.instance.demo;
 
 import com.alibaba.cola.statemachine.StateMachine;
+import com.alibaba.cola.statemachine.builder.AlertFailCallback;
+import com.alibaba.cola.statemachine.builder.FailCallback;
 import com.alibaba.cola.statemachine.builder.StateMachineBuilder;
 import com.alibaba.cola.statemachine.builder.StateMachineBuilderFactory;
+import com.alibaba.cola.statemachine.exception.TransitionFailException;
 import com.alibaba.fastjson2.JSON;
 import com.boommanpro.statemachine.templatefactory.config.TransitionConfigModel;
 import org.apache.commons.io.IOUtils;
@@ -26,6 +29,9 @@ public class OrderStateMachineBuilder {
                     .to(OrderStateEnum.fromCode(model.getTo()))
                     .on(OrderEventEnum.fromCode(model.getEvent()));
         }
+        builder.setFailCallback((sourceState, event, context) -> {
+            throw new TransitionFailException(String.format("状态:[%s](%s) + 事件:[%s](%s) 状态流转失败",sourceState.getText(), sourceState.getCode(), event.getText(),event.getCode()));
+        });
         return builder.build(machineId);
 
     }
