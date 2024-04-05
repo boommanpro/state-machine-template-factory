4c86b77c78ab4a1d7b40a1328dc0b6ab155a45a0
boommanpro <boommanpro@gmail.com>
2024-04-05 11:29:26 +0800

    feat(build state machine server):

---
+++ src/main/java/com/alibaba/cola/statemachine/package-info.java
@@ -0,0 +1,4 @@
+package com.alibaba.cola.statemachine;
+/**
+ * copy from COLA state-machine
+ */
---
+++ src/main/java/com/boommanpro/statemachine/templatefactory/StateMachineTemplateFactoryApplication.java
@@ -0,0 +1,13 @@
+package com.boommanpro.statemachine.templatefactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StateMachineTemplateFactoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StateMachineTemplateFactoryApplication.class, args);
+    }
+
+}
